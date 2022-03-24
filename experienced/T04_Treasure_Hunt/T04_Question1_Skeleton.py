fef65c9d031beaa364df122c12308a91946f3f72
Stefan <stefan.lommaert@student.kuleuven.be>
2022-03-24 20:45:38 +0100

    t1

--- experienced/T04_Treasure_Hunt/T04_Question1_Skeleton.py
+++ experienced/T04_Treasure_Hunt/T04_Question1_Skeleton.py
@@ -6,11 +6,26 @@ It can be seen that the 12th digit of this string is 1.
 If dn represents the nth digit of the string, find the value of the following expression.
 d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 """
+# def getDN():
+#     dn = ''
+#     i = 1
+#     while (len(dn)<1000000):
+#         dn+=str(i)
+#         i+=1
+#     print(i)
+#     return dn
+
+
+def getDN():
+    dn = ''
+    for i in range(1,185188):
+        dn+=str(i)
+    return dn
 
 def get_final_value() -> int:
-    # TODO
-    pass
-
+    dn = getDN()
+    total = int(dn[0]) *int(dn[9])*int(dn[99])*int(dn[999])*int(dn[9999])*int(dn[99999])*int(dn[999999])
+    return total
 ### OUTPUT - DO NOT TOUCH
 print(get_final_value())
 ### END OUTPUT
